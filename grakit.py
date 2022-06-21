fabe9bcf4d5bbf40246a82b2a2d3e9af85a3a504
tao <lennittus@gmail.com>
2022-06-20 20:05:06 -0400

    ver 1.0

---
+++ grakit.py
@@ -0,0 +1,144 @@
+from typing import List, Tuple, Union, Any
+from scipy.spatial import distance
+import torch
+import numpy as np
+
+
+class vertex(object):
+    """
+    A class to represent a vertex.
+
+    ...
+
+    Attributes
+    ----------
+    tag : int
+        tag of the vertex. 
+        e.g. 0 indicates a frame, 1 indicates an object, 2 indicates a wall
+    pos : torch.Tensor
+        3D position
+    feat : ANY
+        feature
+
+    Methods
+    -------
+    info(additional=""):
+        Prints the person's name and age.
+    """
+    def __init__(self, tag:int, pos:torch.Tensor, feat: Any) -> None:
+        self.tag = tag # int
+        self.pos = pos # torch.Tensor
+        self.feat = feat # ANY
+
+    def __str__(self) -> str:
+        return 'vertex | tag: {}\n\
+            pos: {}\n\
+                feat: {}'.format(self.tag, self.pos, self.feat)
+
+
+class obj_vertex(vertex):
+    pass
+
+
+def generate_edge(vertices:List[vertex], base:int, method:int, ratio:float) -> np.ndarray:
+    '''
+    Given a set of vertices, generate edge between them, using one edge policy.
+
+    Parameters:
+        vertices: list of vertices
+        base: indicates which attribute of the vertex the policy is based on
+            0 if based on 3D position, 
+            1 if based on feature
+        method: indicates which method to decide whether there is an egde
+            0 if using l2 distance
+            1 if using similarity test
+        ratio: ratio for method
+
+    Returns:
+        edge matrix: classic adjacency matrix but with diagonal being all 0
+    '''
+    if method == 0: # l2 distance
+        num_v = len(vertices)
+
+        if base == 0: # based on pos
+            lst = [v.pos for v in vertices]
+        else: # based on feat
+            lst = [v.feat for v in vertices]
+        _matrix = torch.stack(lst)
+
+        # shape: (num_v, num_v)
+        dis_matrix = distance.cdist(_matrix, _matrix, 'euclidean')
+        average_dis = np.sum(dis_matrix) / (num_v*(num_v - 1))
+        threshold = average_dis * ratio
+        
+        # shape: (num_v, num_v)
+        edge_matrix_raw = dis_matrix <= threshold
+        f_diag = np.ones((num_v, num_v))
+        np.fill_diagonal(f_diag, 0)
+        
+        edge_matrix = np.logical_and(edge_matrix_raw, f_diag)
+        return edge_matrix
+    
+    elif method == 1: # similarity test
+        pass
+
+    else:
+        print('invalid argument')
+        return None
+
+
+class graph(object):
+    """
+    A class to represent a graph (unweighted & indirected). 
+
+    ...
+
+    Attributes
+    ----------
+    vertices : List[vertex]
+        list of integers indicating the indices of the neighbors of the vertex
+
+    Methods
+    -------
+    get_nbs():
+        Return the degree list and neighbor list of vertices
+    """
+    def __init__(self, vertices:List[vertex], base:int, method:int, ratio:float) -> None:
+        self.V = vertices
+        self.V_size = len(vertices)
+        self.E = generate_edge(vertices, base, method, ratio)
+
+    def get_nbs(self) -> Tuple[np.ndarray, List[np.ndarray]]:
+        '''
+        Returns (deg_lst, nbs_lst_lst), where 
+        1) deg_lst == [deg_i] where deg_i is the degree of the i-th vertex; 
+        2) nbs_lst_lst == [nbs_lst_i] where nbs_lst_i is the list of indices of neighbors of the i-th vertex
+
+        Parameters:
+            self
+
+        Returns:
+            Tuple[np.ndarray, List[np.ndarray]]: degree list and neighbor list
+        '''
+        deg_lst = np.sum(self.E, axis=0)
+        nbs_lst_lst = [np.where(self.E[i])[0] for i in range(self.V_size)]
+        return (deg_lst, nbs_lst_lst)
+
+
+def load_object_vertex(raw_vs:List[Tuple], pos_ind:int, feat_ind:int) -> List[vertex]:
+    '''
+    Given a list of tuple representing raw data and return the list of vertices from of it
+
+    Parameters:
+        raw_vs : the raw data for object information
+        pos_ind: position index, indicating which index of the object information to be the position attribute of the vertex
+        feat_ind: feature index, indicating which index of the object information to be the feature attribute of the vertex
+        
+    Returns:
+        List[vertex]: The list of vertex representation
+    '''
+    lst = []
+    for raw_v in raw_vs:
+        vertex_v = vertex(0, raw_v[pos_ind], raw_v[feat_ind])
+        lst.append(vertex_v)
+    return lst
---
+++ util.py
@@ -0,0 +1,202 @@
+import os
+import json
+import grakit
+import random
+from os.path import join, isfile
+from tkinter.messagebox import NO
+from typing import List, Tuple, Dict
+import torch
+
+
+def get_directories(path:str, print=True) -> List[str]:
+    '''
+    Returns the list of directories within the given path
+
+    Parameters:
+        path (str): the given path
+
+    Returns:
+        dir_list (List[str]): the list of directories within the given path
+    '''
+    dir_list = os.listdir(path)
+    
+    if print:
+        print("Directories in '", path, "' :")
+        print(dir_list)
+
+    return dir_list
+
+
+def generate_labels(path:str) -> dict:
+    '''
+    Returns a generated dictionary for rooms classification 
+
+    Parameters:
+        path (str): the path for the text file containng room-scan information,
+                    where each line has scan(s) for one room
+
+    Returns:
+        labels (Dict[str:int]): key: scan; value: label
+    '''
+    with open(path, 'r') as f:
+        rooms = f.read().splitlines()
+    f.close()
+    labels = {}
+    for i, room in enumerate(rooms):
+        scans = room.split()
+        for scan in scans:
+            labels[scan] = i
+    return labels
+
+
+def remove_odd_rooms(rooms:str, excluded:str, rooms_cleaned:str) -> None:
+    '''
+    Remove rooms, in given 'rooms', which contains any scan from 'excluded'. Then save the new/cleaned set
+    of rooms in the same format in 'rooms_cleaned'
+
+    Parameters:
+        rooms (str): the path for given rooms
+        excluded (str): the path for exluded scans
+        rooms_cleaned (str): the path for cleaned rooms
+
+    Returns:
+        None
+    '''
+    with open(excluded, 'r') as excluded_f:
+        excluded_lst = excluded_f.read().splitlines()
+    excluded_f.close()
+
+    with open(rooms, 'r') as rooms_f:
+        rooms_lst = rooms_f.read().splitlines()
+    rooms_f.close()
+    print('number of rooms originally: {}'.format(len(rooms_lst)))
+
+    for room in rooms_lst:
+        scans = room.split()
+        for scan in scans:
+            if scan in excluded_lst: 
+                rooms_lst.remove(room)
+                break
+                
+    print('number of rooms after exclusion: {}'.format(len(rooms_lst)))
+
+    # Test
+    fail_lst = []
+    checked = True
+    for room in rooms_lst:
+        scans = room.split()
+        for scan in scans:
+            if scan in excluded_lst:
+                checked = False
+                fail_lst.append(scan)
+            
+    print(len(fail_lst), fail_lst)
+    if checked:
+        print('Checked :)')
+    else:
+        print('Fail :(')
+        
+    with open(rooms_cleaned, 'w') as rooms_cleaned_f:
+        rooms_cleaned_f.write('\n'.join(rooms_lst))
+    rooms_cleaned_f.close()
+
+
+def get_object_info_json(path:str) -> List[Tuple]:
+    '''
+    Get object information from .json file
+
+    Parameters:
+        path: the .json file path 
+
+    Returns:
+        list of information of each object in the .json file
+    '''
+    with open(path) as f:
+        data = json.load(f)
+    f.close()
+
+    lst = []
+    for obj in data['segGroups']:
+        pos = torch.tensor(obj['obb']['centroid'])
+        normal = torch.tensor(obj['dominantNormal'])
+        obj_info = (pos, normal)
+        lst.append(obj_info)
+    
+    return lst
+
+
+def build_dataset(rooms_path:str, target_name:str, pos_ind:int, feat_ind:int, base:int, method:int, ratio:float) -> None:
+    '''
+    Build dataset for DGCNN.
+
+    Parameters:
+        rooms_path: the path of the .txt file which stores all rooms
+        target_name: the name of the dataset
+        pos_ind: see @grakit.load_object_vertex
+        feat_ind: see @grakit.load_object_vertex
+        base: see @grakit.generate_edge
+        method: see @grakit.generate_edge
+        ratio: see @grakit.generate_edge
+
+    Returns:
+        None
+    '''
+    data_path = '3rscan'
+    scan_to_label = generate_labels(rooms_path)
+
+    f_path = join('pytorch_DGCNN-master/data', target_name, target_name+'.txt')
+    f = open(f_path, 'w')
+    f.write(str(len(scan_to_label))+'\n')
+    f.close()
+    for scan, label in scan_to_label.items():
+        label = str(scan_to_label[scan])
+        
+        # get obj info
+        path = join(data_path, scan, 'semseg.v2.json')
+        obj_info = get_object_info_json(path)
+
+        # then get obj vertex
+        obj_v = grakit.load_object_vertex(raw_vs=obj_info, pos_ind=pos_ind, feat_ind=feat_ind)
+        obj_g = grakit.graph(vertices=obj_v, base=base, method=method, ratio=ratio)
+        deg_lst = obj_g.get_nbs()[0]
+        nbs_lst = obj_g.get_nbs()[1]
+        
+        f = open(f_path, 'a')
+        f.write(str(len(obj_v)) + ' ' + label + '\n')
+        tag = '0'
+        for i, v in enumerate(obj_v):
+            nbs = " ".join(map(str, nbs_lst[i]))
+            feat = " ".join(map(str, v.feat.numpy()))
+            f.write(" ".join([tag, str(deg_lst[i]), nbs, feat]) + '\n')
+        f.close()
+
+
+def build_cv_split_10fold(data:str, total_size:int, test_size:int) -> None:
+    '''
+    Build cross-validation splite (10 fold) for a dataset
+
+    Parameters:
+        data: the name of the dataset 
+        total_size: the size of the dataset
+        test_size: the size of the test split
+
+    Returns:
+        None
+    '''
+    train_size = total_size - test_size
+    ind_list = [x for x in range(total_size)]
+    random.shuffle(ind_list)
+    for i in range(1,11):
+        test_file = 'test_idx-' + str(i) + '.txt'
+        train_file = 'train_idx-' + str(i) + '.txt'
+        test_path = join('pytorch_DGCNN-master', 'data', data, '10fold_idx', test_file)
+        train_path = join('pytorch_DGCNN-master', 'data', data, '10fold_idx', train_file)
+        random.shuffle(ind_list)
+        with open(train_path, 'w') as f:
+            for ind in ind_list[:train_size]:
+                f.write("{}\n".format(ind))
+        f.close()
+        with open(test_path, 'w') as f:
+            for ind in ind_list[train_size:]:
+                f.write("{}\n".format(ind))
+        f.close()
