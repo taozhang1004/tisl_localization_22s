7c44265dc0416b7fc750e5af425fe6187d58681a
victoria <victoria7kovalenko@gmail.com>
2022-08-04 17:43:35 -0400

    remove unnecessary code from top-n.py

--- top-n.py
+++ top-n.py
@@ -7,21 +7,6 @@ room = data[:, 0]
 scan = np.zeros(room.shape)
 emb = data[:, 1:]
 
-# Initialize numbering.
-# Take advantage of the fact scan is all zeros.
-prev_room = room[0]
-prev_scan = 0
-for i in range(NUM_EMB, emb.shape[0], NUM_EMB):
-    if room[i] == prev_room:
-        # If still same room we just increment scan number
-        prev_scan += 1
-        scan[i:i+NUM_EMB] = prev_scan
-    else:
-        # If new room reset scan counter
-        # Again take advantage that the default value is zero
-        prev_scan = 0
-        prev_room = room[i]
-
 
 top1_count = 0.0
 top5_count = 0.0
