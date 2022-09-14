de41a72c747afeead96baa16755ba74712bc19c8
tao <lennittus@gmail.com>
2022-09-14 13:27:19 -0400

    pass

--- write_netvlad_vectors_for_rooms.py
+++ write_netvlad_vectors_for_rooms.py
@@ -9,7 +9,7 @@ def get_sample_rooms_lst(sample_rooms_path):
     
 def main():
 
-    num_emb = 25
+    num_emb = 50
 
     sample_rooms_path = 'rooms_cleaned_test.txt'
     sample_rooms_lst = get_sample_rooms_lst(sample_rooms_path)
@@ -17,7 +17,7 @@ def main():
     vlad_embeddings_with_room_class_path = f'test_sets/test_set_vlad_embeddings_{num_emb}_emb.txt'
     vlad_embeddings_path = 'pca_vlad_embeddings_50_random_images'
     
-    count = 0 
+    room_number = 0 
     for scans in sample_rooms_lst:
 
         scans_lst = scans.split(" ")
@@ -31,17 +31,19 @@ def main():
                 vlad_embedding_lst = vlad_embedding_file.read().splitlines()
             vlad_embedding_file.close()
 
-            random_vlad_embedding_lst = random.sample(vlad_embedding_lst, num_emb)
-
+            random_vlad_embedding_lst = random.sample(vlad_embedding_lst, min(num_emb, len(vlad_embedding_lst)))
+            print(len(random_vlad_embedding_lst))
             with open(vlad_embeddings_with_room_class_path, 'a') as vlad_embedding_with_room_class_file:
             	for vlad_embedding in random_vlad_embedding_lst:
                     vlad_embedding_without_coordinates = vlad_embedding.split(" ")[3:]
-                    vlad_embedding_with_room_class_file.write(str(count))
+                    vlad_embedding_with_room_class_file.write(str(room_number))
+                    vlad_embedding_with_room_class_file.write(' ')
+                    vlad_embedding_with_room_class_file.write(str(len(random_vlad_embedding_lst)))
                     vlad_embedding_with_room_class_file.write(' ')
                     vlad_embedding_with_room_class_file.write(' '.join(vlad_embedding_without_coordinates))
                     vlad_embedding_with_room_class_file.write('\n')
             vlad_embedding_with_room_class_file.close()
             
-        count +=1 
+        room_number +=1 
         
 main()
